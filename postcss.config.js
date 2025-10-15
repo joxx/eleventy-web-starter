d6c25dfe11be4d66f7b91ddcb2d7c409389e8795
Joachim Kliemann <joachim.kliemann@googlemail.com>
2025-10-15 06:47:42 +0200

    add cube css files

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-	plugins: [
-		require("postcss-import-ext-glob"),
-		require("postcss-import"),
-		require("@tailwindcss/postcss"),
-		require("autoprefixer"),
-		...(process.env.NODE_ENV === "production" ? [require("cssnano")] : []),
-	],
+  plugins: [
+    require('postcss-import-ext-glob'),
+    require('postcss-import'),
+    require('tailwindcss')
+  ]
 };
